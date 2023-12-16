3865cf150de2ae7d62bd803557574f68d6ac5934
Chukwuebuka <chukwuebukaclement@yahoo.com>
2023-12-16 19:50:25 +0100

    add customer registration testing

--- src/main/java/com/ebuka/bankingapi/utils/ValidationUtils.java
+++ src/main/java/com/ebuka/bankingapi/utils/ValidationUtils.java
@@ -1,18 +1,13 @@
 package com.ebuka.bankingapi.utils;
 
-import com.ebuka.bankingapi.model.repository.CustomerRepository;
 import com.ebuka.bankingapi.model.repository.TransactionRepository;
-import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ValidationUtils {
-    public static Principal getAuthenticatedUser() {
-        return (Principal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
+
 
     public static String removeInternationalCode(String phoneNumber) {
         if (phoneNumber.startsWith("+234")) {
